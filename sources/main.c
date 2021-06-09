3933128c871c7af2c09f5665f82791ccade38c42
JSmith604 <jacquelinesmith190@gmail.com>
2021-06-09 22:53:34 +0000

    C practice with variables

--- sources/main.c
+++ sources/main.c
@@ -4,10 +4,12 @@
 
 int main()
 {
-  printf("    /|\n");
-  printf("   / |\n");
-  printf("  /  |\n");
-  printf(" /   |\n");
-  printf("/____|\n");
+  char characterName[] = "Riley";
+  int characterAge = 19;
+  printf("There once was a man named %s\n", characterName);
+  printf("he was %d years old.\n", characterAge);
+  printf("He really liked the name %s\n", characterName);
+  printf("but did not like being %d.\n", characterAge);
+  
   return 0;
 }
